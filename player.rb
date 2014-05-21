d0b17357beda8c680ed46c230543565fbe2783e7
Sergio Perez <sperez4mba@gmail.com>
2014-05-21 14:47:03 -0500

    Refactoring.

--- player.rb
+++ player.rb
@@ -1,8 +1,8 @@
 # player.rb
 class Player 
-  def move(position)
+  def move(position, token)
     board = '---------'
-    board[position - 1] = 'O'
+    board[position - 1] = token 
     return board
   end
 end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -4,10 +4,10 @@ require_relative '../player'
 describe Player, '#nextmove' do
   it "returns board with next move for given position" do
     player = Player.new
-    player.move(2).should eql '-X-------'
+    player.move(2, 'X').should eql '-X-------'
   end
   it "returns board with next move for position 4" do
-    player = Player.new 
-    player.move(4).should eql '---O-----'
+    player = Player.new
+    player.move(4, 'O').should eql '---O-----'
   end
 end
