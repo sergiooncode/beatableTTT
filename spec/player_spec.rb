6e28a50efdafcda4ec4b3508a1203d6422a1f0d0
Sergio Perez <sperez4mba@gmail.com>
2014-05-22 15:50:13 -0500

    Refactoring.

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,13 +1,11 @@
 # player_spec.rb
 require_relative '../player'
 
-describe Player, '#nextmove' do
+describe Player do
   before :each do
     @player = Player.new
   end
-  it "returns board with next move for given position" do
-    @player.move(2, 'X').should eql '-X-------'
-  end
+
   it "returns board with next move for position 4" do
     @player.move(4, 'O').should eql '---O-----'
   end
