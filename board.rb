6b5ef13f3700581a6bcbb82c583a54b3fd19cdb5
Sergio Perez <sperez4mba@gmail.com>
2014-05-22 15:25:47 -0500

    Started over with Board class.

--- board.rb
+++ board.rb
@@ -1,6 +1,12 @@
 # board.rb
 class Board
-  def init
-    return '---------'
-  end  
+  def initialize(size)
+    @squares = String.new	
+    size.times do
+      @squares << '-'
+    end
+  end
+  def get_squares
+    @squares
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,9 +3,10 @@ require_relative '../board'
 
 describe Board do
   before :each do
-    @board = Board.new
+    @board = Board.new(9)
   end
-  it "returns board with empty squares" do
-    @board.init.should eql '---------'
+
+  it "gets empty squares" do
+    @board.get_squares.should eql '---------'
   end
 end
