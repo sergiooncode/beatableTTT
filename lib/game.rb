f6747254c458d86c105c9544ba4a7b6d34fbb4fe
Sergio Perez <sperez4mba@gmail.com>
2014-05-26 17:34:44 -0500

    Tests if there are board and player when game is started.

--- lib/game.rb
+++ lib/game.rb
@@ -1,6 +1,13 @@
+require 'board'
+require 'player'
+
 class Game
+  attr_accessor :board, :player
+
   def initialize(output)
     @output = output
+    @board = Board.new(9)
+    @player = Player.new('X')
   end
   def start
     @output.puts('Welcome to TTT game in Ruby!')
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,12 +1,27 @@
 require 'game'
 
 describe Game do
-  it "sends a welcome message" do
-    output = double('output')
-    game = Game.new(output)
+  context "when new game created" do
+    it "sends a welcome message" do
+      output = double('output')
+      game = Game.new(output)
 
-    expect(output).to receive(:puts).with('Welcome to TTT game in Ruby!') 
+      expect(output).to receive(:puts).with('Welcome to TTT game in Ruby!') 
     
-    game.start
+      game.start
+    end
+    it "has a board" do
+      output = double('output')
+      game = Game.new(output)
+
+      expect(game.board).not_to be_nil
+
+    end
+    it "has a player" do
+      output = double('output')
+      game = Game.new(output)
+
+      expect(game.player).not_to be_nil
+    end
   end
 end
