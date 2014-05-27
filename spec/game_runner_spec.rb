fa5feb602c625af2d82810f0a447e50d584d0034
Sergio Perez <sperez4mba@gmail.com>
2014-05-27 16:36:01 -0500

    Refactoring.

--- spec/game_runner_spec.rb
+++ spec/game_runner_spec.rb
@@ -2,44 +2,34 @@ require 'game_runner'
 
 describe GameRunner do
   context "when new game runner created" do
+    let(:game_runner){ described_class.new  }
     it "has a game object" do
-      game_runner = GameRunner.new
-
       expect(game_runner.game).not_to be_nil
     end
   end
   context "when game is ongoing" do
+    let(:game_runner){ described_class.new  }
     it "calls ask_for_move through game instance" do
-      game_runner = GameRunner.new
-
       expect(game_runner.game).to receive(:ask_for_move)
 
       game_runner.game.ask_for_move
     end
     it "calls make_move through game instance" do
-      game_runner = GameRunner.new
-
       expect(game_runner.game).to receive(:make_move)
 
       game_runner.game.make_move
     end
     it "asks human player for move and makes it" do
-      game_runner = GameRunner.new
-
       expect(game_runner).to receive(:ask_human_player_for_move)
 
       game_runner.ask_human_player_for_move
     end
     it "asks machine player for move and makes it" do
-      game_runner = GameRunner.new
-
       expect(game_runner).to receive(:ask_machine_player_for_move)
 
       game_runner.ask_machine_player_for_move
     end
     it "plays game" do
-      game_runner = GameRunner.new
-
       expect(game_runner).to receive(:run)
 
       game_runner.run
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,42 +2,32 @@ require 'game'
 
 describe Game do
   context "when new game created" do
+    let(:game){ described_class.new }
     it "sends a welcome message" do
-      game = Game.new
-
       expect(game).to receive(:puts).with('Welcome to TTT game in Ruby!') 
     
       game.start
     end
     it "has a board" do
-      game = Game.new
-
       expect(game.board).not_to be_nil
     end
     it "has a player" do
-      game = Game.new
-
       expect(game.player).not_to be_nil
     end
   end
   context "while playing" do
+    let(:game){ described_class.new }
     it "asks human player for move one time" do
-      game = Game.new
-
       expect(game).to receive(:ask_human_for_move)
 
       game.ask_human_for_move
     end
     it "asks machine player for move one time" do
-      game = Game.new
-
       expect(game).to receive(:ask_machine_for_move)
 
       game.ask_machine_for_move
     end
     it "makes next move calling move method on player instance with position 5" do
-      game = Game.new
-
       expect(game.make_move(5)).to eql('----X----')
     end
   end
