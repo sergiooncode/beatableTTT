09623f4aeabdaf15037636da08c7ee707b99541c
Sergio Perez <sperez4mba@gmail.com>
2014-05-28 10:24:15 -0500

    Added three more tests to check all rows.

--- lib/game_ref.rb
+++ lib/game_ref.rb
@@ -1,5 +1,18 @@
 class GameRef
   def check_on_rows(board)
+    squares = board.get_squares
+    if !board.is_square_empty(1) && squares[0] == squares[1] && squares[0] == squares[2] 
+      # winner = squares[0]
+      return true
+    end
+    if !board.is_square_empty(4) && squares[3] == squares[4] && squares[3] == squares[5]
+      # winner = squares[3]
+      return true
+    end
+    if !board.is_square_empty(7) && squares[6] == squares[7] && squares[6] == squares[8]
+      # winner = squares[6]
+      return true
+    end
     false
   end
 end
--- spec/game_ref_spec.rb
+++ spec/game_ref_spec.rb
@@ -1,10 +1,37 @@
 require 'game_ref'
 
 describe GameRef do
-  it "checks if winner on rows and there is none" do
-    board = Board.new(9)
-    game_ref = GameRef.new
+  context "when checking rows for winner" do
+    let(:game_ref){ described_class.new }
+    it "checks if winner on rows and there is none" do
+      board = Board.new(9)
 
-    expect(game_ref.check_on_rows(board)).not_to be_true
+      expect(game_ref.check_on_rows(board)).to be_false
+    end
+    it "checks if winner on 1st row and there is one" do
+      board = Board.new(9)
+
+      board.set_square(1, 'X')
+      board.set_square(2, 'X')
+      board.set_square(3, 'X')
+
+      expect(game_ref.check_on_rows(board)).to be_true
+    end
+    it "checks if winner on 2nd row and there is one" do
+      board = Board.new(9)
+      
+      board.set_square(4, 'X')
+      board.set_square(5, 'X')
+      board.set_square(6, 'X')
+
+      expect(game_ref.check_on_rows(board)).to be_true
+    end
+    it "checks if winner on 3rd row and there is one" do
+      board = Board.new(9)
+      
+      board.set_square(7, 'X')
+      board.set_square(8, 'X')
+      board.set_square(9, 'X')
+    end
   end
 end
