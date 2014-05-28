384fbc070648a51caf34e759e2e365ae96ed9ee3
Sergio Perez <sperez4mba@gmail.com>
2014-05-28 12:15:12 -0500

    Added methods to check columns and diagonals for winner.

--- lib/game_ref.rb
+++ lib/game_ref.rb
@@ -11,4 +11,27 @@ class GameRef
     end
     false
   end
+  def check_on_columns(board)
+    squares = board.squares
+    start_column = 0
+    while start_column < 3
+      if !board.is_square_empty(start_column) && squares[start_column] == squares[start_column + 3] && squares[start_column] == squares[start_column + 6]
+        return true
+      end
+      start_column += 1
+    end
+    false
+  end
+  def check_on_diagonals(board)
+    squares = board.squares
+    start_diagonal = 0
+    if !board.is_square_empty(start_diagonal) && squares[start_diagonal] == squares[start_diagonal + 4] && squares[start_diagonal] == squares[start_diagonal + 4]
+      return true
+    end
+    start_diagonal += 6
+    if !board.is_square_empty(start_diagonal) && squares[start_diagonal] == squares[start_diagonal - 2] && squares[start_diagonal] == squares[start_diagonal - 4]
+      return true
+    end
+    false
+  end
 end
--- spec/game_ref_spec.rb
+++ spec/game_ref_spec.rb
@@ -36,4 +36,55 @@ describe GameRef do
       expect(game_ref.check_on_rows(board)).to be_true
     end
   end
+  context "when checking columns for winner" do
+    let(:game_ref){ described_class.new }
+    it "checks if winner on 1st column and there is one" do
+      board = Board.new(9)
+
+      board.squares[0] = 'O'
+      board.squares[3] = 'O'
+      board.squares[6] = 'O'
+
+      expect(game_ref.check_on_columns(board)).to be_true
+    end
+    it "check if winner on 2nd column and there is one" do
+      board = Board.new(9)
+
+      board.squares[1] = 'O'
+      board.squares[4] = 'O'
+      board.squares[7] = 'O'
+
+      expect(game_ref.check_on_columns(board)).to be_true
+    end
+    it "check if winner on 3rd column and there is one" do
+      board = Board.new(9)
+
+      board.squares[2] = 'O'
+      board.squares[5] = 'O'
+      board.squares[8] = 'O'
+
+      expect(game_ref.check_on_columns(board)).to be_true
+    end
+  end
+  context "when checking diagonals for winner" do
+    let(:game_ref){ described_class.new }
+    it "check if winner on N-S diagonal" do
+      board = Board.new(9)
+
+      board.squares[0] = 'X'
+      board.squares[4] = 'X'
+      board.squares[8] = 'X'
+
+      expect(game_ref.check_on_diagonals(board)).to be_true
+    end
+    it "check if winner on S-N diagonal" do
+      board = Board.new(9)
+
+      board.squares[2] = 'X'
+      board.squares[4] = 'X'
+      board.squares[6] = 'X'
+
+      expect(game_ref.check_on_diagonals(board)).to be_true
+    end
+  end
 end
