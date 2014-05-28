254d22e2d18c1dd985f4753b24644b7cc01f9287
Sergio Perez <sperez4mba@gmail.com>
2014-05-28 11:35:42 -0500

    Refactoring.

--- lib/game_ref.rb
+++ lib/game_ref.rb
@@ -1,17 +1,13 @@
 class GameRef
   def check_on_rows(board)
-    squares = board.get_squares
-    if !board.is_square_empty(1) && squares[0] == squares[1] && squares[0] == squares[2] 
-      # winner = squares[0]
-      return true
-    end
-    if !board.is_square_empty(4) && squares[3] == squares[4] && squares[3] == squares[5]
-      # winner = squares[3]
-      return true
-    end
-    if !board.is_square_empty(7) && squares[6] == squares[7] && squares[6] == squares[8]
-      # winner = squares[6]
-      return true
+    squares = board.squares
+    start_row = 0
+    while start_row < 7
+      if !board.is_square_empty(start_row) && squares[start_row] == squares[start_row + 1] && squares[start_row] == squares[start_row + 2] 
+        # winner = squares[0]
+        return true
+      end
+      start_row += 3
     end
     false
   end
--- spec/game_ref_spec.rb
+++ spec/game_ref_spec.rb
@@ -11,27 +11,29 @@ describe GameRef do
     it "checks if winner on 1st row and there is one" do
       board = Board.new(9)
 
-      board.set_square(1, 'X')
-      board.set_square(2, 'X')
-      board.set_square(3, 'X')
+      board.squares[0] = 'X'
+      board.squares[1] = 'X'
+      board.squares[2] = 'X'
 
       expect(game_ref.check_on_rows(board)).to be_true
     end
     it "checks if winner on 2nd row and there is one" do
       board = Board.new(9)
       
-      board.set_square(4, 'X')
-      board.set_square(5, 'X')
-      board.set_square(6, 'X')
+      board.squares[3] = 'X'
+      board.squares[4] = 'X'
+      board.squares[5] = 'X'
 
       expect(game_ref.check_on_rows(board)).to be_true
     end
     it "checks if winner on 3rd row and there is one" do
       board = Board.new(9)
       
-      board.set_square(7, 'X')
-      board.set_square(8, 'X')
-      board.set_square(9, 'X')
+      board.squares[6] = 'X'
+      board.squares[7] = 'X'
+      board.squares[8] = 'X'
+
+      expect(game_ref.check_on_rows(board)).to be_true
     end
   end
 end
